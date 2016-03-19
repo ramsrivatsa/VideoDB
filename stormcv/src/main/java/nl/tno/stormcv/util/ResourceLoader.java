5b6b4770ab2ac7477d4499029a9a2f8bdbc22dce
Aetf <aetf@unlimitedcodeworks.xyz>
2016-03-18 21:06:35 -0400

    Make ResourceLoader thread-safe

--- stormcv/src/main/java/nl/tno/stormcv/util/ResourceLoader.java
+++ stormcv/src/main/java/nl/tno/stormcv/util/ResourceLoader.java
@@ -3,11 +3,16 @@ package nl.tno.stormcv.util;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * A thread safe resource loader
+ *
  * Created by Aetf (aetf at unlimitedcodeworks dot xyz) on 16-3-12.
  */
 public class ResourceLoader {
@@ -18,23 +23,30 @@ public class ResourceLoader {
     public static File inflateResource(String resPath, boolean deleteOnExit) {
         InputStream in = null;
         try {
-            in = ResourceLoader.class.getResourceAsStream(resPath);
             String filename = new File(resPath).getName();
-
-            //String[] tokens = filename.split("\\.(?=[^\\.]+$)");
-            //Path tempFile = Files.createTempFile(tokens[0], tokens[1], fileAttributes);
             Path tempPath = getTempDirectory().resolve(filename);
             File tempFile = tempPath.toFile();
 
-            if (tempFile.exists())
+            Object lock = new Object();
+            synchronized (lock) {
+                Object prev = inflatedResource.putIfAbsent(tempPath.toString(), lock);
+                if (prev != null) {
+                    synchronized (prev) {
+                        return tempFile;
+                    }
+                }
+
+                in = ResourceLoader.class.getResourceAsStream(resPath);
+                try {
+                    Files.copy(in, tempPath, StandardCopyOption.REPLACE_EXISTING);
+                } catch (FileAlreadyExistsException e) {
+                }
+
+                if (deleteOnExit)
+                    tempFile.deleteOnExit();
+
                 return tempFile;
-
-            if (deleteOnExit)
-                tempFile.deleteOnExit();
-
-            Files.copy(in, tempPath, StandardCopyOption.REPLACE_EXISTING);
-
-            return tempFile;
+            }
         } catch (Exception e) {
             throw new RuntimeException("Failed to load resource: " + resPath, e);
         } finally {
@@ -44,9 +56,10 @@ public class ResourceLoader {
             }
         }
     }
+    private static Map<String, Object> inflatedResource = new ConcurrentHashMap<>();
 
     private static Path tempDirectory = null;
-    private static Path getTempDirectory() throws IOException {
+    private static synchronized Path getTempDirectory() throws IOException {
         if (tempDirectory != null)
             return tempDirectory;
         tempDirectory = Files.createTempDirectory("cvld");

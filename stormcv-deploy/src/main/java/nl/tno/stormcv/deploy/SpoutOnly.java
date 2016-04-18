bb933c142a5b7e26d29b684845cf9d93b1859201
Aetf <aetf@unlimitedcodeworks.xyz>
2016-04-17 23:52:30 -0400

    Add SpoutOnly topology

---
+++ stormcv-deploy/src/main/java/nl/tno/stormcv/deploy/SpoutOnly.java
@@ -0,0 +1,112 @@
+package nl.tno.stormcv.deploy;
+
+import backtype.storm.Config;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+import nl.tno.stormcv.StormCVConfig;
+import nl.tno.stormcv.fetcher.FileFrameFetcher;
+import nl.tno.stormcv.model.Frame;
+import nl.tno.stormcv.operation.DnnClassifyOp;
+import nl.tno.stormcv.operation.DnnForwardOp;
+import nl.tno.stormcv.operation.HaarCascadeOp;
+import nl.tno.stormcv.operation.ISingleInputOperation;
+import nl.tno.stormcv.spout.CVParticleSpout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Aetf (aetf at unlimitedcodeworks dot xyz) on 16-3-19.
+ */
+public class SpoutOnly {
+    public static void main(String[] args) {
+        // process args
+        final String switchKeyword = "--";
+        int maxSpoutPending = 128;
+        int msgTimeout = 25;
+        int cacheTimeout = 30;
+        List<String> files = new ArrayList<>();
+        for (String arg : args) {
+            if (arg.startsWith(switchKeyword)) {
+                String[] kv = arg.substring(switchKeyword.length()).split("=");
+                if (kv.length != 2) continue;
+                int value;
+                try {
+                    value = Integer.parseInt(kv[1]);
+                } catch (NumberFormatException ex) {
+                    continue;
+                }
+                switch (kv[0]) {
+                    case "max-spout-pending":
+                        maxSpoutPending = value;
+                        break;
+                    case "msg-timeout":
+                        msgTimeout = value;
+                        break;
+                    case "cache-timeout":
+                        cacheTimeout = value;
+                        break;
+                }
+            } else {
+                // Multiple files will be spread over the available spouts
+                files.add("file://" + arg);
+            }
+        }
+
+        // first some global (topology configuration)
+        StormCVConfig conf = new StormCVConfig();
+
+        // number of workers in the topology
+        conf.setNumWorkers(2);
+        // maximum un-acked/un-failed frames per spout (spout blocks if this number is reached)
+        conf.setMaxSpoutPending(maxSpoutPending);
+        // indicates frames will be encoded as JPG throughout the topology
+        conf.put(StormCVConfig.STORMCV_FRAME_ENCODING, Frame.JPG_IMAGE);
+        // True if Storm should timeout messages or not.
+        conf.put(Config.TOPOLOGY_ENABLE_MESSAGE_TIMEOUTS, true);
+        // The maximum amount of time given to the topology to fully process a message emitted by a spout (default = 30)
+        conf.put(Config.TOPOLOGY_MESSAGE_TIMEOUT_SECS, msgTimeout);
+        // indicates if the spout must be fault tolerant
+        conf.put(StormCVConfig.STORMCV_SPOUT_FAULTTOLERANT, true);
+        // TTL (seconds) for all elements in all caches throughout the topology (avoids memory overload)
+        conf.put(StormCVConfig.STORMCV_CACHES_TIMEOUT_SEC, cacheTimeout);
+
+        // Internal message buffers
+        //conf.put(Config.TOPOLOGY_TRANSFER_BUFFER_SIZE,            32);
+        //conf.put(Config.TOPOLOGY_EXECUTOR_RECEIVE_BUFFER_SIZE, 16384);
+        //conf.put(Config.TOPOLOGY_EXECUTOR_SEND_BUFFER_SIZE,    16384);
+
+
+        // Enable time profiling for spout and bolt
+        conf.put(StormCVConfig.STORMCV_LOG_PROFILING, true);
+
+        // specify the list with SingleInputOperations to be executed sequentially by the 'fat' bolt
+        List<ISingleInputOperation> operations = new ArrayList<>();
+        operations.add(new HaarCascadeOp("face", "haarcascade_frontalface_default.xml"));
+        operations.add(new DnnForwardOp("classprob", "/data/bvlc_googlenet.prototxt", "/data/bvlc_googlenet.caffemodel").outputFrame(true));
+        operations.add(new DnnClassifyOp("classprob", "/data/synset_words.txt").addMetadata(true).outputFrame(true));
+        //operations.add(new FeatureExtractionOp("sift", FeatureDetector.SIFT, DescriptorExtractor.SIFT));
+
+        int frameSkip = 1;
+        // now create the topology itself
+        // (spout -> scale -> fat[face detection & dnn] -> drawer -> streamer)
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("fetcher", new CVParticleSpout(
+                        new FileFrameFetcher(files).frameSkip(frameSkip)),
+                1);
+
+        try {
+
+            // run in local mode
+            //LocalCluster cluster = new LocalCluster();
+            //cluster.submitTopology("spout_only", conf, builder.createTopology());
+            //Utils.sleep(120 * 1000); // run for two minutes and then kill the topology
+            //cluster.shutdown();
+
+            // run on a storm cluster
+            StormSubmitter.submitTopology("spout_only", conf, builder.createTopology());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

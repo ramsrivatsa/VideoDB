40fbed36f2cad561d31cff6870322ee120df269e
Aetf <aetf@unlimitedcodeworks.xyz>
2016-05-31 17:45:22 -0400

    Fix Timing.currentTimeMillis() to be related to wall-clock

--- stormcv/src/main/java/xyz/unlimitedcodeworks/utils/Timing.java
+++ stormcv/src/main/java/xyz/unlimitedcodeworks/utils/Timing.java
@@ -4,7 +4,21 @@ package xyz.unlimitedcodeworks.utils;
  * Created by Aetf (aetf at unlimitedcodeworks dot xyz) on 16-5-31.
  */
 public class Timing {
+    static {
+        adjustment = millisFromSys() - millisFromNano();
+    }
+
     public static long currentTimeMillis() {
+        return millisFromNano() + adjustment;
+    }
+
+    static long millisFromNano() {
         return System.nanoTime() / 1000000;
     }
+
+    static long millisFromSys() {
+        return System.currentTimeMillis();
+    }
+
+    public static long adjustment = 0;
 }

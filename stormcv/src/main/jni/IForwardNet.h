54f5670d10587f5bc51bc7b590249874b6a93f41
Aetf <aetf@unlimitedcodeworks.xyz>
2016-06-20 11:43:46 -0400

    Fix compiler warning

--- stormcv/src/main/jni/IForwardNet.h
+++ stormcv/src/main/jni/IForwardNet.h
@@ -8,9 +8,9 @@
 namespace ucw {
 class IForwardNet {
 public:
-    std::vector<cv::Mat> forward(const std::vector<cv::Mat>& imgs) = 0;
+    virtual std::vector<cv::Mat> forward(const std::vector<cv::Mat>& imgs) = 0;
 
-    cv::Mat forward(const cv::Mat &input) = 0;
+    virtual cv::Mat forward(const cv::Mat &input) = 0;
 
     virtual ~IForwardNet() {}
 };
